600a46f1ad20df7f2b9e9acc675cb62100ea3c46
Jhonatan da Silva <jhonatan.s04@aluno.ifsc.edu.br>
2017-01-20 19:14:04 -0200

    update

---
+++ 01-printing/printing.py
@@ -0,0 +1,7 @@
+print("Esse é um teste")
+x = 2
+print("o valor de x é : %d" %x)
+print("o valor de x é : ", x)
+y = 3 
+print("o valor de x é : ", x, "o valor de y é : ",y)
+print("o valor de x é {} o valor de y é {}".format(x,y))
