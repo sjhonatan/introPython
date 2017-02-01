78014107c9852b9f2a6768c9eafe0236a0c66222
Jhonatan da Silva <jhonatan.s04@aluno.ifsc.edu.br>
2017-02-01 19:26:04 -0200

    update

--- 00-hello/hello.py
+++ 00-hello/hello.py
@@ -5,6 +5,7 @@ Tue Jan 31 22:35:53 2017
 Number of code lines: 
 3
 """
+#test
 # Lecture on youtube 
 # https://www.youtube.com/playlist?list=PLvu-cXEstYRPZXyug9IqTJq4JtdBxXgeS
 print("Hello world!")
