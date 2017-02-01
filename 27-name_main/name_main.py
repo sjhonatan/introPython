c2254e41c308c2eaa617b3c4b921c63d776feb0e
Jhonatan da Silva <jhonatan.s04@aluno.ifsc.edu.br>
2017-01-31 22:24:54 -0200

    update

---
+++ 27-name_main/name_main.py
@@ -0,0 +1,17 @@
+def main():
+    printA()
+    printB()
+    printC()
+
+def printA():
+    print('A')
+
+def printB():
+    print('B')
+
+def printC():
+    print('C')
+
+if __name__ == '__main__':  
+    print(__name__)
+    main()
